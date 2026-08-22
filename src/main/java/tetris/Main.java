4ea7ab230eb39a3d81be077a76fa5e1d73bfe492
baggaanmol <anmolbagga1202@gmail.com>
2026-08-22 19:22:02 +0300

    initial commit: milestone 1

---
+++ src/main/java/tetris/Main.java
@@ -0,0 +1,10 @@
+package tetris;
+
+import tetris.application.TetrisApplication;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(TetrisApplication.class, args);
+    }
+}
